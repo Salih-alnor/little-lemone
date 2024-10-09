f4216fd6b5409b0386eb28f3227f5edddedc618e
Salih-alnor <salih.gad@icloud.com>
2024-10-09 13:41:46 +0300

    new change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello in little lemone</Text>
+      <Text>hello in little lemone app</Text>
       <StatusBar style="auto" />
     </View>
   );
